8556085eb1935a53b555e95208f82d4292d69ee9
xj63 <xj63work@163.com>
2024-12-18 18:19:20 +0800

    feat: record list auto scroll to bottom.

--- src/components/TimeRecords.vue
+++ src/components/TimeRecords.vue
@@ -11,23 +11,44 @@
       />
     </ul>
     <div class="clear-button-container">
-      <DeleteButton @click="records = []" />
+      <DeleteButton @click="clearRecords" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, nextTick, watch, onMounted, useTemplateRef } from "vue";
 import RecordItem from "./RecordItem.vue";
 import DeleteButton from "./DeleteButton.vue";
 
 const records = ref([]);
+const recordList = useTemplateRef("recordList");
 
 const timediff = (now, last) => (last ? now.time - last.time : 0);
 
 const removeRecord = (index) => records.value.splice(index, 1);
 
-const addRecord = () => records.value.push({ time: new Date(), note: "" });
+const scrollToBottom = () => {
+  if (recordList.value)
+    recordList.value.scrollTop = recordList.value.scrollHeight;
+};
+
+watch(records, () => {
+  nextTick(scrollToBottom);
+});
+
+const clearRecords = () => {
+  records.value = [];
+};
+
+onMounted(() => {
+  scrollToBottom();
+});
+
+const addRecord = () => {
+  records.value.push({ time: new Date(), note: "" });
+  nextTick(scrollToBottom);
+};
 const isEmpty = computed(() => records.value.length === 0);
 
 defineExpose({ addRecord, isEmpty });
