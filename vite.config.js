383321109dc1ed4f5978fd33bb45be12347a92a6
xj63 <xj63work@163.com>
2024-12-27 12:55:12 +0800

    feat: use vite-plugin-sitemap to add sitemap for website.

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Sitemap({ hostname: 'https://ticktime.xj63.fun' }),
   ],
   resolve: {
     alias: {
